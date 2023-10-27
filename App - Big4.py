beb255597a2e4a9b4aa0586514af59e9cef3da82
Vince1993 <52530147+Vince1993@users.noreply.github.com>
2023-10-27 18:16:43 +1300

    Add files via upload

---
+++ App - Big4.py
@@ -0,0 +1,28 @@
+print("""End meat eating:
+Buying and eating meat causes damage to the earth.
+Furthermore animals need to be killed by someone.
+My suggestion is to eat no meat. Or less. Or at least stop spending money on it.
+Changing your diet, causes great impact on your body.
+""")
+
+print("""Ride the bicycle, not the car:
+Take your bike more often, not the car.
+Saves you money and you are out in nature.
+""")
+
+print("""Stop buying things:
+What do you really need?
+Food, accomodation, warmth, clothing
+""")
+
+
+print("""Stop buying things:
+What do you really need?
+Food, accomodation, warmth, clothing
+""")
+
+print("""Avoid litter.
+Markets not supermarkets.
+Avoid plastics.
+Avoid litter in general, no coffee to go is a good start.
+""")
